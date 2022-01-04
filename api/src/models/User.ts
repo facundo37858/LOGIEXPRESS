bf94eafca27162d8f890952095645283da3b1d6c
facundo2309 <facundoomargarcia@gmail.com>
2022-01-04 18:03:56 -0300

    datos

--- api/src/models/User.ts
+++ api/src/models/User.ts
@@ -1,9 +1,14 @@
 
 import {Model,Column,Table,CreatedAt,UpdatedAt} from 'sequelize-typescript'
+
+import {Model,Column,Table, PrimaryKey, AutoIncrement, DataType} from 'sequelize-typescript'
+
 import {v4 as uuid} from 'uuid'
+
 
 @Table
 export class User extends Model<User>{
+
     // @Column(DataType.UUIDV4)
     // @PrimaryKey                //no se si es correcto pq en realidad en una FK q proviene de User_Reg al determinar la realacion
     // id:string=uuid()

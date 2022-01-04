1f9e98d88a18d7bfaca3556bd827fa58fb9d6bdc
facundo2309 <facundoomargarcia@gmail.com>
2022-01-04 18:02:45 -0300

    id autogenerado con uuid
    prmary key

--- api/src/models/User.ts
+++ api/src/models/User.ts
@@ -4,6 +4,12 @@ import {v4 as uuid} from 'uuid'
 
 @Table
 export class User extends Model<User>{
+    // @Column(DataType.UUIDV4)
+    // @PrimaryKey                //no se si es correcto pq en realidad en una FK q proviene de User_Reg al determinar la realacion
+    // id:string=uuid()
+    @PrimaryKey
+    @Column
+    id:string=uuid()
     @Column
     name!:string
 

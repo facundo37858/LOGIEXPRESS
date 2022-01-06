1e0561d6e8ce29c5c3f830d73a066995f9bb6732
Luis Rivera <rluis747@gmail.com>
2022-01-05 20:50:49 -0500

    Dependencies install

--- clientnative/App.js
+++ clientnative/App.js
@@ -1,42 +1,23 @@
-<<<<<<< HEAD
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-=======
 import React from "react";
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import SingIn from "./Components/SingIn";
-import SingUp from "./Components/SingUp"; 
->>>>>>> 6d118004e1a107fc053cea0a52e3a6781fe03c06
+import SingUp from "./Components/SingUp";
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-<<<<<<< HEAD
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-=======
-  <NavigationContainer>
-    <Stack.Navigator >
-      <Stack.Screen name="singIn" component={SingIn} options={{headerShown:false}} />
-      <Stack.Screen name="singUp" component={SingUp} />
-    </Stack.Navigator>
-  </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="singIn"
+          component={SingIn}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="singUp" component={SingUp} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 export default App;
->>>>>>> 6d118004e1a107fc053cea0a52e3a6781fe03c06

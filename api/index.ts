d2c471170d3031f9301934217678775a3c4226fc
alexk8uu <fredyalejandrocarrizo@gmail.com>
2022-01-24 14:52:14 -0300

    merge con Eliana

--- api/index.ts
+++ api/index.ts
@@ -98,7 +98,7 @@ sequelize
     // })
     .then(() => {
         console.log('base de datos conectada! :D')
-        server.listen(process.env.PORT || 3000, function () {
+        server.listen(3001, function () {
             console.log('App is listening on port 3001!');
         });
     })
